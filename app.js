7b4a3aa2557ee54aedcbf294687a3473cb216743
Lapapatota <engel.mini1048@gmail.com>
2026-09-22 21:19:22 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 const video = document.getElementById("camara");
-const mensaje = document.getElementById("mensaje");
 
-async function iniciarCamara() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-            video: {
-                facingMode: "user"
-            },
-            audio: false
-        });
-
-        video.srcObject = stream;
-
-        video.onloadedmetadata = () => {
-            video.play();
-            mensaje.textContent = "📷 Cámara activada";
-        };
-
-    } catch (error) {
-        console.error("Error de cámara:", error);
-
-        mensaje.textContent =
-            "❌ No se pudo acceder a la cámara";
-
-        alert("Error al abrir la cámara: " + error.message);
-    }
-}
-
-iniciarCamara();
+navigator.mediaDevices.getUserMedia({
+    video: {
+        facingMode: "environment"
+    },
+    audio: false
+})
+.then(stream => {
+    video.srcObject = stream;
+})
+.catch(error => {
+    alert("Error: " + error.name + " - " + error.message);
+});
